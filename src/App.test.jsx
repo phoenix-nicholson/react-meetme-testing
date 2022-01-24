71c0f73f1405f4d4ba2867dea302862c6d9dd658
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-01-24 13:05:45 -0800

    writing tests for header image and alt text, taking lunch

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,16 @@
+import { screen, render, waitForElementToBeRemoved } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import App from './App'
 
 test('Should render the header', () => {
+  render(<App />)
 
+  const heading = screen.getByRole('heading', {
+    name: /Alchemy Logo/i,
+  })
+
+  const logo = screen.getByAltText(/Alchemy Logo/i)
+
+  expect(heading).toBeInTheDocument()
+  expect(logo).toBeInTheDocument()
 })
